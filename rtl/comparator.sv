eb86a1274ca3390f101b59b5047f3b9429877bab
valmyr <valmir.silva@ee.ufcg.edu.br>
2026-06-27 01:25:32 -0300

    Chore: Renomeando variável credito para credit

--- rtl/comparator.sv
+++ rtl/comparator.sv
@@ -1,8 +1,8 @@
 module comparator(
-    input  logic [7:0] credito,
+    input  logic [7:0] credit,
     input  logic [7:0] price,
     input  logic [7:0] stock,
     output logic [7:0] can_sell
 );  
-assign can_sell = (credito >= price && stock > 0);
+assign can_sell = (credit >= price && stock > 0);
 endmodule
