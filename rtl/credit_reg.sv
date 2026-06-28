3d2aa37361c63422c4e4fcd404acb63a0f4998af
valmyr <valmir.silva@ee.ufcg.edu.br>
2026-06-28 16:28:34 -0300

    Fix: apagando initial, e iniciando a memoria via reset

--- rtl/credit_reg.sv
+++ rtl/credit_reg.sv
@@ -7,17 +7,15 @@ module credit_reg(
 );  
 
 
-logic [7:0] mem [3:0]; // 4 Palavras de 8 bits cada
-initial begin
-    // Inicializa a memória com os valores desejados   ]
-    mem[0] = 8'h00; // Moeda de 00 centavos  
-    mem[1] = 8'h25; // Moeda de 25 centavos
-    mem[2] = 8'h50; // Moeda de 50 centavos
-    mem[3] = 8'h100; // Moeda de 100 centavos
-end     
+logic [7:0] mem [3:0]; // 4 Palavras de 8 bits cada    
 always_ff@(posedge clk)begin
-    if(!rst)begin
+    if(rst)begin
         credit <= 8'b0;
+        // Inicializa a memória com os valores desejados   ]
+        mem[0] = 8'd00; // Moeda de 00 centavos  
+        mem[1] = 8'd25; // Moeda de 25 centavos
+        mem[2] = 8'd50; // Moeda de 50 centavos
+        mem[3] = 8'd100; // Moeda de 100 centavos
     end else begin
         if(credit_load)begin
             credit <= credit + mem[coin_value];
