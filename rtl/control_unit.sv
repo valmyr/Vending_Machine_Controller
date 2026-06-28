34921aa0605959fbc3ed540bc6d8cdcbc36d3ee2
valmyr <valmir.silva@ee.ufcg.edu.br>
2026-06-28 19:44:01 -0300

    Feat: Criando modelo para a integracao

---
+++ rtl/control_unit.sv
@@ -0,0 +1,41 @@
+module control_vending_machine(
+    input logic clk,
+    input logic rst,
+    input logic cancel,
+    input logic [1:0]coin_in,
+    input logic confirm,
+    input logic sel_item,
+
+
+    output logic error,
+    output logic dispense,
+    output logic [7:0] change_out,
+    output logic [7:0] display,
+    output logic [2:0] state_out,
+
+    //Sinais internos
+
+    //memory
+    input  logic [7:0] mem_inst_stock,
+    input  logic [7:0] mem_inst_price,
+    output logic       mem_inst_mem_read,
+    output logic       mem_inst_mem_write,
+    output logic       mem_inst_sel_item,
+    //Comparator
+    output logic [7:0] comp_inst_credit,
+    output logic [7:0] comp_inst_price,
+    output logic [7:0] comp_inst_stock,
+    input  logic [7:0] comp_inst_can_sell,
+    //credit_reg
+    output logic       credit_inst_credit_load,
+    output logic [1:0] credit_inst_coin_value,
+    input  logic [7:0] credit_inst_credit,
+    //Subtractor
+    output logic [7:0] sub_inst_credit,
+    output logic [7:0] sub_inst_price,
+    input  logic [7:0] sub_inst_change
+
+);
+//Continue
+
+endmodule
