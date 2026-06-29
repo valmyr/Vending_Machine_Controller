3a2682c5a54c3d8fd9a203b4b8189263a40d8f36
valmyr <valmir.silva@ee.ufcg.edu.br>
2026-06-28 23:19:30 -0300

    BugFix: Correcao de atribuicao bloqueante para nao bloqueante

--- rtl/memory.sv
+++ rtl/memory.sv
@@ -16,10 +16,10 @@ always_ff@(posedge clk)begin
         stock <= 8'b0;
         price <= 8'b0;
         // Inicializa a memória com os valores desejados
-        memory_array[0] = 16'h1905; // Produto 0 - Café : Preço 25  centavos, Estoque 5
-        memory_array[1] = 16'h3205; // Produto 1 - Água : Preço 50  centavos, Estoque 5
-        memory_array[2] = 16'h4B03; // Produto 2 - Suco : Preço 75  centavos, Estoque 3
-        memory_array[3] = 16'h6402; // Produto 3 - Snack: Preço 100 centavos, Estoque 2
+        memory_array[0] <= 16'h1905; // Produto 0 - Café : Preço 25  centavos, Estoque 5
+        memory_array[1] <= 16'h3205; // Produto 1 - Água : Preço 50  centavos, Estoque 5
+        memory_array[2] <= 16'h4B03; // Produto 2 - Suco : Preço 75  centavos, Estoque 3
+        memory_array[3] <= 16'h6402; // Produto 3 - Snack: Preço 100 centavos, Estoque 2
     end else begin
         if(mem_read)begin
             stock <= memory_array[sel_item][7:0];
