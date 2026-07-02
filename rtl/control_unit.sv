9d6b736ad95cebc699ce5b90a253ba3a1463c242
Marcos Luz <marcos.antonioluz36@gmail.com>
2026-07-02 18:57:41 -0300

    feat: maquina de estados implementada

--- rtl/control_unit.sv
+++ rtl/control_unit.sv
@@ -1,43 +1,120 @@
-module control_vending_machine(
-    input logic clk,
-    input logic rst,
-    input logic cancel,
-    input logic [1:0]coin_in,
-    input logic confirm,
-    input logic [1:0]sel_item,
+module control_unit(
+    input  wire        clk,
+    input  wire        rst,
+ 
+    input  wire        can_sell,
+    input  wire  [1:0] coin_in,
+    input  wire        confirm,
+    input  wire        cancel,
 
+    output logic       credit_load,
+    output logic       en_change,
+    output logic       mem_write,
+    output logic       mem_read,
+    output logic       clr,
 
-    output logic error,
-    output logic dispense,
-    output logic [7:0] change_out,
-    output logic [7:0] display,
-    output logic [2:0] state_out,
-
-    //Sinais internos
-
-    //memory
-    input  logic [7:0] mem_inst_stock,
-    input  logic [7:0] mem_inst_price,
-    output logic       mem_inst_mem_read,
-    output logic       mem_inst_mem_write,
-    output logic  [1:0]mem_inst_sel_item,
-    //Comparator
-    output logic [7:0] comp_inst_credit,
-    output logic [7:0] comp_inst_price,
-    output logic [7:0] comp_inst_stock,
-    input  logic [7:0] comp_inst_can_sell,
-    //credit_reg
-    output logic       credit_inst_credit_load,
-    output logic [1:0] credit_inst_coin_value,
-    input  logic [7:0] credit_inst_credit,
-    //Subtractor
-    output logic [7:0] sub_inst_credit,
-    output logic [7:0] sub_inst_price,
-    input  logic [7:0] sub_inst_change
-
+    output logic [3:0] state_out,
+    output logic       dispense,
+    output logic       error
 );
 
 import Vending_Machine_pkg::*;
-//Continue
+
+fsm_state_t state, next_state;
+
+always_ff @ (posedge clk) begin
+    if (rst) begin
+        state <= IDLE;
+    end
+
+    else begin
+        state <= next_state;
+    end
+end
+
+always_comb begin
+    next_state = state;
+    credit_load = 0;
+    mem_write = 0;
+    mem_read = 0;
+
+    en_change = 0;
+    dispense = 0;
+    error = 0;
+    clr = 0;
+
+    state_out = state;
+
+    case (state)
+        IDLE: begin
+            if (|coin_in != 0) begin
+                clr = 0;
+                credit_load = 1'b1;
+                next_state = COLLECT;
+            end
+
+            else begin
+                clr = 1'b1;
+                next_state = IDLE;
+            end
+        end
+
+        COLLECT:begin
+            credit_load = |coin_in;
+
+            if (cancel) begin
+                next_state = IDLE;
+            end
+
+            else if (confirm) begin
+                next_state = CHECK;
+            end
+
+            else begin
+                next_state = COLLECT;
+            end
+        end
+
+        CHECK: begin
+            mem_read = 1'b1;
+
+            if (can_sell) begin
+                next_state = DISPENSE;
+            end
+
+            else begin
+                next_state = ERROR;
+            end
+        end
+
+        DISPENSE: begin
+            dispense = 1'b1;
+            mem_write = 1'b1;
+            next_state = CHANGE;
+        end
+
+        CHANGE: begin
+            en_change = 1'b1;
+            next_state = IDLE;
+        end
+
+        ERROR: begin
+            error = 1'b1;
+            
+            if (cancel) begin
+                en_change = 1'b1;
+                next_state = IDLE; 
+            end
+
+            else begin
+                next_state = ERROR;
+            end
+        end
+
+        default: begin
+            next_state = IDLE;
+        end
+    endcase
+end
 
 endmodule
