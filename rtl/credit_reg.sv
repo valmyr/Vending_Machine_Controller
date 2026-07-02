9fa397de21185c3e03685802ebe8fc3ebd7e6496
Marcos Luz <marcos.antonioluz36@gmail.com>
2026-07-02 19:00:28 -0300

    feat: ajustes nos modulos topo e credit_reg para trabalhar com a maquina de estados

--- rtl/credit_reg.sv
+++ rtl/credit_reg.sv
@@ -1,22 +1,25 @@
 module credit_reg(
-    input  logic clk,
-    input  logic rst,
-    input  logic credit_load,
+    input  wire        clk,
+    input  wire        rst,
+    input  wire        clr,
+    input  wire        credit_load,
     input  logic [1:0] coin_value,
     output logic [7:0] credit
 );  
 
-
 logic [7:0] mem [3:0]; // 4 Palavras de 8 bits cada    
-always_ff@(posedge clk)begin
-    if(rst)begin
+
+always_ff @ (posedge clk) begin
+    if(rst || clr) begin
         credit <= 8'b0;
         // Inicializa a memória com os valores desejados   ]
-        mem[0] = 8'd00; // Moeda de 00 centavos  
-        mem[1] = 8'd25; // Moeda de 25 centavos
-        mem[2] = 8'd50; // Moeda de 50 centavos
-        mem[3] = 8'd100; // Moeda de 100 centavos
-    end else begin
+        mem[0] <= 8'd00; // Moeda de 00 centavos  
+        mem[1] <= 8'd25; // Moeda de 25 centavos
+        mem[2] <= 8'd50; // Moeda de 50 centavos
+        mem[3] <= 8'd100; // Moeda de 100 centavos
+    end 
+    
+    else begin
         if(credit_load)begin
             credit <= credit + mem[coin_value];
         end
--- rtl/vending_top.sv
+++ rtl/vending_top.sv
@@ -1,118 +1,109 @@
 module Vending_Top(
-    input  logic clk,
-    input  logic rst,
+    input  logic       clk,
+    input  logic       rst,
 
     // Inputs
-    input  logic confirm,
-    input  logic cancel,
     input  logic [1:0] sel_item,
     input  logic [1:0] coin_in,
+    input  logic       confirm,
+    input  logic       cancel,
     // Outputs
-    output logic dispense,  
-    output logic error,
     output logic [7:0] change_out,
-    output logic [7:0] display,
-    output logic [2:0] state_out
+    output logic [2:0] state_out,
+    output logic       dispense,
+    output logic [7:0] display,  
+    output logic       error
 );
 // Instanciação dos sinais internos do Top Level
 //====================================================================
     //memory signals
-    logic [7:0] mem_inst_stock;
-    logic [7:0] mem_inst_price;
-    logic       mem_inst_mem_read;
-    logic       mem_inst_mem_write;
-    logic [1:0] mem_inst_sel_item;
+    logic [7:0] stock;
+    logic [7:0] price;
+    logic       mem_read;
+    logic       mem_write;
+    logic [1:0] sel_item;
+    logic       en_change;
     
     //credit_reg signals
-    logic       credit_inst_credit_load;
-    logic [7:0] credit_inst_credit;
-    logic [1:0] credit_inst_coin_value;
+    logic       credit_load;
+    logic [7:0] credit;
+    logic [1:0] coin_value;
 
     //comparator signals
-    logic [7:0] comp_inst_credit;
-    logic [7:0] comp_inst_price;
-    logic [7:0] comp_inst_stock;
-    logic [7:0] comp_inst_can_sell;
-
-    //subtractor signals
-    logic [7:0] sub_inst_credit;
-    logic [7:0] sub_inst_price;
-    logic [7:0] sub_inst_change;
+    logic       can_sell;
+    logic [7:0] change;
 //=================================================================
 
-    // Instanciação dos módulos
+    assign display = credit;
 
+    always @ (posedge clk) begin
+        if (rst) begin
+            change_out <= 0;
+        end
+
+        else begin
+            change_out <= en_change ? change : change_out;
+        end
+    end
+
+    // Instanciação dos módulos
     //Memoria
     memory u_mem_unit(
-        .clk        (clk),
-        .rst        (rst),
-        .sel_item   (mem_inst_sel_item),
-        .stock      (mem_inst_stock),
-        .price      (mem_inst_price),
-        .mem_read   (mem_inst_mem_read),
-        .mem_write  (mem_inst_mem_write)
+        .clk(clk),
+        .rst(rst),
+
+        .stock(stock),
+        .price(price),
+        .sel_item(sel_item),
+        
+        .mem_read(mem_read),
+        .mem_write(mem_write)
     );
 
     //Registardor de Creditos
     credit_reg u_credit_unit(
-        .clk         (clk),
-        .rst         (rst),
-        .credit_load (credit_inst_credit_load),
-        .coin_value  (credit_inst_coin_value),
-        .credit      (credit_inst_credit)
+        .clk(clk),
+        .rst(rst),
+        .clr(clr),
+
+        .credit(credit),
+        .coin_value(coin_value),
+        .credit_load(credit_load)
     );
 
     //Comparador
     comparator u_comp_unit(
-        .credit  (comp_inst_credit),
-        .price   (comp_inst_price),
-        .stock   (comp_inst_stock),
-        .can_sell(comp_inst_can_sell)
+        .price(price),
+        .stock(stock),
+        .credit(credit),
+        .can_sell(can_sell)
     );
 
     //Subtrator
     subtractor u_sub_unit(
-        .credit(sub_inst_credit),
-        .price (sub_inst_price),
-        .change(sub_inst_change)
+        .price (price),
+        .credit(credit),
+        .change(change)
     );  
 
     // Instanciação da Unit de Controle (FSM)
-    control_vending_machine u_control_unit(
-        //Sinais globais
+    control_unit u_control_unit(
         .clk(clk),
         .rst(rst),
-        //Sinais externos
-        .cancel(cancel),
+
+        .can_sell(can_sell),
         .coin_in(coin_in),
-        .sel_item(sel_item),
         .confirm(confirm),
-        .error(error),
-        .dispense(dispense),
-        .change_out(change_out),
-        .display(display),
+        .cancel(cancel),
+
+        .credit_load(credit_load),
+        .mem_write(mem_write),
+        .mem_read(mem_read),
+
+        .en_change(en_change),
         .state_out(state_out),
-        //Sinais internos
-
-        //memory
-        .mem_inst_stock(mem_inst_stock),
-        .mem_inst_price(mem_inst_price),
-        .mem_inst_mem_read(mem_inst_mem_read),
-        .mem_inst_mem_write(mem_inst_mem_write),
-        .mem_inst_sel_item(mem_inst_sel_item),
-        //Comparator
-        .comp_inst_credit(comp_inst_credit),
-        .comp_inst_price(comp_inst_price),
-        .comp_inst_stock(comp_inst_stock),
-        .comp_inst_can_sell(comp_inst_can_sell),
-        //credit_reg
-        .credit_inst_credit_load(credit_inst_credit_load),
-        .credit_inst_coin_value(credit_inst_coin_value),
-        .credit_inst_credit(credit_inst_credit),
-        //Subtractor
-        .sub_inst_credit(sub_inst_credit),
-        .sub_inst_price(sub_inst_price),
-        .sub_inst_change(sub_inst_change)
-
+        .dispense(dispense),
+        .error(error),
+        .clr(clr)
     );
 endmodule
