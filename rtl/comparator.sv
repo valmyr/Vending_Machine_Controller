e607e39db6f30012702ec996b54d657eda8d35bb
valmyr <valmir.silva@ee.ufcg.edu.br>
2026-07-17 18:57:19 -0300

    chore: correcao de tamanhos de entradas

--- rtl/comparator.sv
+++ rtl/comparator.sv
@@ -2,7 +2,7 @@ module comparator(
     input  logic [7:0] credit,
     input  logic [7:0] price,
     input  logic [7:0] stock,
-    output logic [7:0] can_sell
+    output logic  can_sell
 );  
 assign can_sell = (credit >= price && stock > 0);
 endmodule
